a13a0fd9d1b5c6eb81aba35f73aa5ce5b5176ede
Petr Kurapov <petr.a.kurapov@intel.com>
2023-06-12 13:18:40 -0500

    Add level zero support by default

--- recipe/build-hdk.sh
+++ recipe/build-hdk.sh
@@ -31,6 +31,7 @@ cmake -Wno-dev \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX/$INSTALL_BASE \
     -DCMAKE_BUILD_TYPE=Release \
+    -DENABLE_L0=on \
     -DENABLE_AWS_S3=off -B build -S .
 
 cmake --build build --parallel $(nproc)
